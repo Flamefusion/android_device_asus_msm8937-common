2a5dda1502af430ff3cd80be0017ad39d9f79aff
Michael Bestas <mkbestas@lineageos.org>
2019-09-26 01:56:06 +0300

    msm8937-common: Switch to device/qcom/sepolicy-legacy-um policy
    Change-Id: I09ee12755ab0a1f54a9dca4b92313ef1127db3d6

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -191,7 +191,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 TARGET_RECOVERY_FSTAB := $(VENDOR_PATH)/rootdir/fstab.qcom
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
 # Treble
