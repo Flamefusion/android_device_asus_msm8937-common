b7f1af68db4c21327a078ab805957369fb3a575b
Flamefusion <meshekharbehera613@gmail.com>
2020-05-24 00:59:30 +0530

    Update pa_X00P.mk

--- pa_X00P.mk
+++ pa_X00P.mk
@@ -25,19 +25,26 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 
 # Inherit some common AOSPA stuff.
-$(call inherit-product, vendor/pa/config/common_full_phone.mk)
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_NOGAPPS := true
+TARGET_INCLUDE_WIFI_EXT := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+BLISS_BUILDTYPE := UNOFFICIAL
+BLISS_DEVELOPER := Flamefusion
 
-TARGET_ARCH := arm64
-TARGET_DENSITY := xhdpi
+
+
+
 
 # Check for the target product. Inherit from X00P device
 $(call inherit-product, device/asus/X00P/device.mk)
 
-$(call inherit-product, device/qcom/common/common.mk)
+
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := X00P
-PRODUCT_NAME := pa_X00P
+PRODUCT_NAME := bliss_X00P
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := ASUS_X00PD
 PRODUCT_MANUFACTURER := asus
@@ -49,6 +56,11 @@ TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 2280
 
 PRODUCT_GMS_CLIENTID_BASE := android-asus
+
+# Device Maintainer
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.bliss.maintainer= Flamefusion
+
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="WW_X00PD-user 8.0.0 OPR1.170623.032 15.00.1801.31 release-keys"
